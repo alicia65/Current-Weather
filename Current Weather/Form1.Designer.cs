18e6780b4d58fb3742eaf9562338603482c62771
alicia65 <alicia65>
2020-02-23 01:23:04 -0600

    Add project files.

---
+++ Current Weather/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Current_Weather
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnGetWeather = new System.Windows.Forms.Button();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtState = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.picWeather = new System.Windows.Forms.PictureBox();
+            this.lblWeather = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picWeather)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "City:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(269, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "State:";
+            // 
+            // btnGetWeather
+            // 
+            this.btnGetWeather.Location = new System.Drawing.Point(525, 22);
+            this.btnGetWeather.Name = "btnGetWeather";
+            this.btnGetWeather.Size = new System.Drawing.Size(119, 23);
+            this.btnGetWeather.TabIndex = 2;
+            this.btnGetWeather.Text = "Get Weather";
+            this.btnGetWeather.UseVisualStyleBackColor = true;
+            this.btnGetWeather.Click += new System.EventHandler(this.btnGetWeather_Click);
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(81, 19);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(155, 22);
+            this.txtCity.TabIndex = 0;
+            // 
+            // txtState
+            // 
+            this.txtState.Location = new System.Drawing.Point(344, 22);
+            this.txtState.Name = "txtState";
+            this.txtState.Size = new System.Drawing.Size(159, 22);
+            this.txtState.TabIndex = 1;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(540, 404);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(84, 23);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // picWeather
+            // 
+            this.picWeather.Location = new System.Drawing.Point(48, 78);
+            this.picWeather.Name = "picWeather";
+            this.picWeather.Size = new System.Drawing.Size(638, 320);
+            this.picWeather.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.picWeather.TabIndex = 7;
+            this.picWeather.TabStop = false;
+            this.picWeather.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // lblWeather
+            // 
+            this.lblWeather.Location = new System.Drawing.Point(118, 99);
+            this.lblWeather.Name = "lblWeather";
+            this.lblWeather.Size = new System.Drawing.Size(429, 71);
+            this.lblWeather.TabIndex = 8;
+            this.lblWeather.Text = "Enter city and state to get to get weather";
+            this.lblWeather.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnGetWeather;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 450);
+            this.Controls.Add(this.lblWeather);
+            this.Controls.Add(this.picWeather);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.txtState);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.btnGetWeather);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Current Weather";
+            ((System.ComponentModel.ISupportInitialize)(this.picWeather)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnGetWeather;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtState;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.PictureBox picWeather;
+        private System.Windows.Forms.Label lblWeather;
+    }
+}
+
